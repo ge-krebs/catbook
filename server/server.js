0fa0508cbb54984d0618e2197b9fabaa4e5a12ca
Gemma Krebs <96151596+ge-krebs@users.noreply.github.com>
2023-05-15 15:45:11 +1200

    add search and name compliment feature

--- server/server.js
+++ server/server.js
@@ -6,10 +6,14 @@ module.exports = server
 
 server.use(express.static('server/public'))
 
+server.use(express.urlencoded({extended: false}))
+
 server.get('/compliment', (req, res) => {
   res.send('<h1>you look <em>nice</em> today</h1>')
 })
 
+//get profile by search name
+// http://localhost:3000/profile?name=silvia
 server.get('/profile', (req, res) => {
   const name = req.query.name
   if(name == 'silvia') {
@@ -19,6 +23,8 @@ server.get('/profile', (req, res) => {
   }
 })
 
+//get profiles by ID
+//http://localhost:3000/profiles/2
 server.get('/profiles/:id', (req, res) => {
   const id = req.params.id
   if(id === '1') {
@@ -28,4 +34,13 @@ server.get('/profiles/:id', (req, res) => {
   }
 })
 
-server.get('/search', (req, res))
+//get name search/homepage
+//http://localhost:3000/getname
+server.get('/getname', (req, res) => {
+  res.sendFile(__dirname + '/public/get-name.html')
+})
+
+//post named compliment
+server.post('/named-compliment', (req, res) => {
+  res.send('<h1>' + req.body.name + ', you look beautiful today <3</h1>')
+})
