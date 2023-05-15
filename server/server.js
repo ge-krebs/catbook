c3f635b09afc832b98787cabcd2896ec22ea519d
Gemma Krebs <96151596+ge-krebs@users.noreply.github.com>
2023-05-15 12:19:32 +1200

    add public folder and connect css files + imgs

--- server/server.js
+++ server/server.js
@@ -4,24 +4,26 @@ const server = express()
 
 module.exports = server
 
+server.use(express.static('server/public'))
+
 server.get('/compliment', (req, res) => {
   res.send('<h1>you look <em>nice</em> today</h1>')
 })
 
-// server.get('/profile', (req, res) => {
-//   const name = req.query.name
-//   if(name == 'silvia') {
-//   res.sendFile(__dirname + '/silvia.html')
-//   } if(name == 'sampson') {
-//     res.sendFile(__dirname + '/sampson.html')
-//   }
-// })
+server.get('/profile', (req, res) => {
+  const name = req.query.name
+  if(name == 'silvia') {
+  res.sendFile(__dirname + '/silvia.html')
+  } if(name == 'sampson') {
+    res.sendFile(__dirname + '/sampson.html')
+  }
+})
 
-server.get('/profiles', (req, res) => {
+server.get('/profiles/:id', (req, res) => {
   const id = req.params.id
-  if(id === 1) {
+  if(id === '1') {
     res.sendFile(__dirname + '/silvia.html')
-  } if(id === 2) {
+  } if(id === '2') {
     res.sendFile(__dirname + '/sampson.html')
   }
 })
