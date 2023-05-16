24390d019ca449bdbcdd129654fc4ae2f7b478f4
Gemma Krebs <gemmakrebs@live.com>
2023-05-16 21:07:13 +1200

    amend files to hbs, css updates and routing updates

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const express = require('express')
 //file upload
-const fileUpload = require('express-fileupload');
+const fileUpload = require('express-fileupload')
+//hbs express
+const hbs = require('express-handlebars')
 
 const server = express()
-
 module.exports = server
 
-server.use(express.static('server/public'))
+//middleware
+server.engine(
+  'hbs',
+  hbs.engine({
+    extname: 'hbs',
+  })
+)
+server.set('view engine', 'hbs')
+server.set('views', __dirname + '/views')
+server.use(express.static(__dirname + '/public'))
 
-server.use(express.urlencoded({extended: false}))
+// server.use(express.static('server/public'))
+
+server.use(express.urlencoded({ extended: false }))
 //file upload
 server.use(fileUpload())
 
@@ -16,24 +28,29 @@ server.get('/compliment', (req, res) => {
   res.send('<h1>you look <em>nice</em> today</h1>')
 })
 
+// R O U T E S //
+
+//main page//
+server.get('/', (req, res) => {
+  const template = 'home'
+  res.render(template)
+})
+
 //get profile by search name
 // http://localhost:3000/profile?name=silvia
 server.get('/profile', (req, res) => {
   const name = req.query.name
-  if(name == 'silvia') {
-  res.sendFile(__dirname + '/silvia.html')
-  } if(name == 'sampson') {
-    res.sendFile(__dirname + '/sampson.html')
-  }
+  res.sendFile(__dirname + '/' + name + '.html')
 })
 
 //get profiles by ID
 //http://localhost:3000/profiles/2
 server.get('/profiles/:id', (req, res) => {
   const id = req.params.id
-  if(id === '1') {
+  if (id === '1') {
     res.sendFile(__dirname + '/silvia.html')
-  } if(id === '2') {
+  }
+  if (id === '2') {
     res.sendFile(__dirname + '/sampson.html')
   }
 })
@@ -52,8 +69,8 @@ server.post('/named-compliment', (req, res) => {
 // https://pqina.nl/blog/upload-image-with-nodejs/
 // upload images to public?
 server.post('/upload', (req, res) => {
-  const {image} = req.files;
-  if(!image) return res.sendStatus(400)
+  const { image } = req.files
+  if (!image) return res.sendStatus(400)
 
   //move image to public folder
   image.mv(__dirname + '/public/' + image.name)
